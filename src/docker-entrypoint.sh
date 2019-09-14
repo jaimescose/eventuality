024121f8847cffb296806a75cd7d7c454f71996e
Jaime Escobar <jjescobar@uninorte.edu.co>
2019-09-14 11:14:37 -0500

    fix and create init.sh for docker

---
+++ src/docker-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+while !</dev/tcp/db/5432
+do 
+    echo "Waiting for postgres server"
+    sleep 1
+done
+
+python manage.py makemigrations
+
+python manage.py migrate
+
+python manage.py collectstatic --no-input
+
+gunicorn eventuality.wsgi -b 0.0.0.0:8000
