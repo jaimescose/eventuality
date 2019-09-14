d1c320a6f6fbf4b19e138477ccd1b1dcaf8126d3
Jaime Escobar <jjescobar@uninorte.edu.co>
2019-09-13 23:08:59 -0500

    docker and settings fixes

---
+++ database/init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 -U "postgres" -d "postgres" <<-EOSQL
+	CREATE USER "$USER" WITH ENCRYPTED PASSWORD '$PASSWORD';
+	CREATE DATABASE "$DATABASE" OWNER "$USER";
+EOSQL
