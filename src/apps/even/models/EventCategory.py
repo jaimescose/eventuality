556d235cc4e29b5f6bd15fb9c08f2b13300a6515
Jaime Escobar <jjescobar@uninorte.edu.co>
2019-10-06 20:52:16 -0500

    update event category model

--- src/apps/even/models/EventCategory.py
+++ src/apps/even/models/EventCategory.py
@@ -1,25 +1,12 @@
 from django.db import models
 
 class EventCategory(models.Model):
-    CATEOGRIES = [
-        (0, 'Music'),
-        (1, 'Film'),
-        (2, 'Lectures & Books'),
-        (3, 'Fashion'),
-        (4, 'Music'),
-        (5, 'Visual Arts'),
-        (6, 'Performing Arts'),
-        (7, 'Film'),
-        (8, 'Lectures & Books'),
-        (9, 'Fashion'),
-        (10, 'Food & Drink'),
-        (11, 'Festivals & Fairs'),
-        (12, 'Charities'),
-        (13, 'Sports & Active Life'),
-        (15, 'Nightlife'),
-        (16, 'Kids & Family'),
-        (17, 'Other')
-    ]
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
 
-    name = models.PositiveSmallIntegerField(choices=CATEOGRIES)
-    description = models.TextField()
+    class Meta:
+        verbose_name = 'Event Category'
+        verbose_name_plural = 'Event Categories'
+
+    def __str__(self):
+        return self.name
