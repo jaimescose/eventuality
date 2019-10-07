a859d8e6b8cc05c805e9cd1d607ae2d1bfe5238b
Jaime Escobar <jjescobar@uninorte.edu.co>
2019-10-06 22:28:42 -0500

    add parent categpry field

--- src/apps/even/admin/EventCategoryAdmin.py
+++ src/apps/even/admin/EventCategoryAdmin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from apps.even.models.EventCategory import EventCategory
 
 class EventCategoryAdmin(admin.ModelAdmin):
-    list_display = ['name', 'description']
+    list_display = ['name', 'description', 'parent_category']
     ordering = ['name']
 
 admin.site.register(EventCategory, EventCategoryAdmin)
