70d0411923519a1165d0263e506a86b3309fa5de
Jaime Escobar <jjescobar@uninorte.edu.co>
2019-10-22 23:51:47 -0500

    update creating category method

--- src/apps/even/scripts/createCategoriesFromExcel.py
+++ src/apps/even/scripts/createCategoriesFromExcel.py
@@ -20,7 +20,11 @@ categories_df = pandas.read_excel(categories_file)
 
 categories_df.fillna('', inplace=True)
 
+updated = 0
+created = 0
 for index, row in categories_df.iterrows():
+    category_name = row['name']
+
     # Set category description
     description = row['description']
     additional_text = row['additional text']
@@ -28,15 +32,10 @@ for index, row in categories_df.iterrows():
     if additional_text != '':
         description = '\n'.join([description, additional_text])
 
-    # Set-up initial data for category
-    new_category = {
-        'name': row['name'],
-        'description': description
-    }
-
     # Get parent category name
-    parent_category_name = name = row['parent']
+    parent_category_name = row['parent']
 
+    parent_category = None
     if parent_category_name != '':
         # Get parent category
         try:
@@ -44,7 +43,36 @@ for index, row in categories_df.iterrows():
         except EventCategory.DoesNotExist:
             capture_exception('Parent category {} for {} subcategory has not been created'.format(
                 parent_category_name, new_category.name))
-        else:
-            new_category['parent_category'] = parent_category
 
-    EventCategory.objects.create(**new_category)
+    # Check if a category already exists with that name
+    try:
+        existing_category = EventCategory.objects.get(name = category_name)
+    except EventCategory.DoesNotExist as e:
+        # Set-up initial data for category
+        new_category = {
+            'name': category_name,
+            'description': description,
+            'parent_category': parent_category
+        }
+
+        EventCategory.objects.create(**new_category)
+
+        created = created + 1
+    else:
+        is_updated = False
+        # Update existing category
+        if existing_category.description != description:
+            existing_category.description = description
+            is_updated = True
+
+        if existing_category.parent_category != parent_category:
+            existing_category.parent_category = parent_category
+            is_updated = True
+
+        if is_updated:
+            existing_category.save()
+            updated = updated + 1
+
+print('{} categories were created'.format(created))
+
+print('{} categories were updated'.format(updated))
