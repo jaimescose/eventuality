dd01bcc594e1b48bba57065226f69055445de6ca
Jaime Escobar <jjescobar@uninorte.edu.co>
2019-11-09 15:30:47 -0500

    add default field in category

--- src/apps/even/models/eventCategory.py
+++ src/apps/even/models/eventCategory.py
@@ -4,6 +4,7 @@ from django.db import models
 class EventCategory(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
+    default = models.BooleanField(default=False)
     parent_category = models.ForeignKey(
         'self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
 
