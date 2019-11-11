a7879f500482bfbb0dec6018c22af396ce03f16d
Jaime Escobar <jjescobar@uninorte.edu.co>
2019-11-10 21:16:51 -0500

    alter profile model

--- src/apps/users/models/userProfile.py
+++ src/apps/users/models/userProfile.py
@@ -4,7 +4,8 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    birthdate = models.DateTimeField()
+    bio = models.TextField(max_length=500, default='')
+    birthdate = models.DateTimeField(null=True, blank=True, default=None)
 
     def __str__(self):
         return format(self.user.username)
